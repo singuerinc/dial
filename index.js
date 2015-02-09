d3639dfbd72408d527f21c040cb23b16cbceeca6
Nahuel Scotti <nahuel.scotti@gmail.com>
2015-02-10 00:56:13 +0100

    change to angular, search added

--- index.js
+++ index.js
@@ -1,92 +1,55 @@
-// var style = document.createElement('style');
-// var head = document.querySelector('head');
-// var r = function(){ return Math.floor(Math.random() * 255) };
-// style.textContent = '#clock { color: rgb('+r()+', '+r()+', '+r()+') }';
-// head.appendChild(style);
+angular.module('dial', [])
+    .controller('DialController', ['$scope', '$filter', function ($scope, $filter) {
 
+        $scope.search = '';
+        $scope.data = data;
+        $scope.theme = 'theme_dark';
 
-(function(){
+        if(typeof(localstorage))
 
-    var Dial = function(){
+        $scope.time = '-';
 
-    };
-
-    Dial.prototype.init = function(){
-
-        this.body = $('body');
-        this.wrapper = $('.wrapper');
-
-        this._construct(data);
+        $scope.changeTheme = function (theme) {
+            $scope.theme = theme;
+            localStorage.setItem('theme', theme);
+        };
 
         var theme = localStorage.getItem('theme');
         if(typeof(theme) != 'undefined'){
-            this.changeTheme(theme);
+            $scope.changeTheme(theme);
         }
 
-        var pckry = new Packery( '.wrapper', {
-            itemSelector: 'section',
-            gutter: 0
-        });
-
-        this._update_time();
-        setInterval(this._update_time, 1000);
-
-        this._light_theme_btn = $('#theme-light');
-        this._dark_theme_btn = $('#theme-dark');
-
-        this._light_theme_btn.on('click', this._changeThemeToLight.bind(this));
-        this._dark_theme_btn.on('click', this._changeThemeToDark.bind(this));
-
-        return this;
-    };
-
-    Dial.prototype._construct = function(data){
-
-        for(var i=0; i<data.length; i++){
-            var className = data[i].title.toLowerCase().replace(/\s/ig, '_');
-            var section = $('<section class="'+className+'"><h1>'+data[i].title+'</h1></section>');
-
-            var level2 = data[i].links;
-            var ul = $('<ul></ul>');
-            for(var j=0; j<level2.length; j++){
-                var item = level2[j];
-                var li = $('<li><a href="'+item.href+'">'+item.label+'</a></li>');
-                li.appendTo(ul);
+        $scope.navigate = function($event){
+            if($event.charCode == 13){
+                //console.log($scope.search);
+                var resultLevel1 = $filter('filter')(data, $scope.search, 'label');
+                if (resultLevel1.length > 0) {
+                    var result = $filter('filter')(resultLevel1[0].links, $scope.search, 'label');
+                    if (result.length > 0) {
+                        document.location.href = result[0].href;
+                    }
+                }
             }
-            ul.appendTo(section);
-            section.appendTo(this.wrapper);
-        }
-    };
+        };
 
-    Dial.prototype._changeThemeToLight = function(){
-        this.changeTheme('theme_light');
-    };
+        setTimeout(function () {
 
-    Dial.prototype._changeThemeToDark = function(){
-        this.changeTheme('theme_dark');
-    };
+            new Packery('.wrapper', {
+                itemSelector: 'section',
+                gutter: 0
+            });
 
-    Dial.prototype.changeTheme = function(theme){
-        this.body.attr('class', '');
-        this.body.addClass(theme);
-        localStorage.setItem('theme', theme);
-        return this;
-    };
+        }, 1);
 
-    Dial.prototype._update_time = function(){
-        var today=new Date();
-        var d = today.getDate();
-        var M = today.getMonth()+1;
-        var YYYY = today.getFullYear();
-        var h=today.getHours();
-        var m=today.getMinutes();
-        m = m < 10 ? '0'+m : m;
-        // document.getElementById('clock').innerHTML = h+":"+m+" <small>"+d+"-"+M+"-"+YYYY+"</small> ";
-        document.getElementById('clock').innerHTML = h+":"+m;
+        var _update_time = function () {
+            var today = new Date();
+            var h = today.getHours();
+            var m = today.getMinutes();
+            m = m < 10 ? '0' + m : m;
+            $scope.time = h + ":" + m;
+        };
 
-    };
+        _update_time();
+        setInterval(_update_time, 1000);
 
-    var dial = new Dial()
-        .init();
-
-})();
+    }]);
