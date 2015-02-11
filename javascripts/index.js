f885bfd448c6148ab9d1533c72f4d2e44c77ca95
Nahuel Scotti <nahuel.scotti@gmail.com>
2015-02-11 22:11:23 +0100

    options link

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,77 +1,79 @@
 angular.module('dial', [])
-  .controller('DialController', ['$scope', '$filter', function($scope, $filter) {
+	.controller('DialController', ['$scope', '$filter', function($scope, $filter) {
 
-    var layout = function() {
-      new Packery('.wrapper', {
-        itemSelector: 'section',
-        gutter: 0
-      });
-    };
+		var layout = function() {
+			new Packery('.wrapper', {
+				itemSelector: 'section',
+				gutter: 0
+			});
+		};
 
-    var _onDataLoaded = function(items) {
-      $scope.$apply(function() {
+		var _onDataLoaded = function(items) {
+			$scope.$apply(function() {
 
-        $scope.data = JSON.parse(items.dial_data);
+				$scope.data = JSON.parse(items.dial_data);
 
-        var all_data = [];
-        for (var i = 0; i < $scope.data.length; i++) {
-          var links = $scope.data[i].links;
-          for (var j = 0; j < links.length; j++) {
-            all_data.push(links[j]);
-          }
-        }
+				var all_data = [];
+				for (var i = 0; i < $scope.data.length; i++) {
+					var links = $scope.data[i].links;
+					for (var j = 0; j < links.length; j++) {
+						all_data.push(links[j]);
+					}
+				}
 
-        $scope.all_data = all_data;
-        setTimeout(layout, 1);
-      });
-    };
+				$scope.all_data = all_data;
+				setTimeout(layout, 1);
+			});
+		};
 
-    chrome.storage.sync.get('dial_data', _onDataLoaded);
-    //_onDataLoaded(data);
+		chrome.storage.sync.get('dial_data', _onDataLoaded);
+		//_onDataLoaded(data);
 
-    $scope.search = '';
+		$scope.search = '';
 
-    $scope.theme = 'theme_dark';
+		$scope.theme = 'theme_dark';
 
-    $scope.time = '-';
+		$scope.time = '-';
 
-    $scope.init = function() {
-      document.getElementById('search').focus();
-    };
+		$scope.init = function() {
+			document.getElementById('search').focus();
+		};
 
-    $scope.changeTheme = function(theme) {
-      $scope.theme = theme;
-      localStorage.setItem('theme', theme);
-    };
+		$('#options_url').attr('href', chrome.extension.getURL("options.html"));
 
-    var theme = localStorage.getItem('theme');
-    if (typeof(theme) != 'undefined') {
-      $scope.changeTheme(theme);
-    }
+		$scope.changeTheme = function(theme) {
+			$scope.theme = theme;
+			localStorage.setItem('theme', theme);
+		};
 
-    $scope.navigate = function($event) {
-      if ($event.charCode == 13) {
-        //console.log($scope.search);
-        var resultLevel1 = $filter('filter')(data, $scope.search, 'label');
-        if (resultLevel1.length > 0) {
-          var result = $filter('filter')(resultLevel1[0].links, $scope.search,
-            'label');
-          if (result.length > 0) {
-            document.location.href = result[0].href;
-          }
-        }
-      }
-    };
+		var theme = localStorage.getItem('theme');
+		if (typeof(theme) != 'undefined') {
+			$scope.changeTheme(theme);
+		}
 
-    var _update_time = function() {
-      var today = new Date();
-      var h = today.getHours();
-      var m = today.getMinutes();
-      m = m < 10 ? '0' + m : m;
-      $scope.time = h + ":" + m;
-    };
+		$scope.navigate = function($event) {
+			if ($event.charCode == 13) {
+				//console.log($scope.search);
+				var resultLevel1 = $filter('filter')(data, $scope.search, 'label');
+				if (resultLevel1.length > 0) {
+					var result = $filter('filter')(resultLevel1[0].links, $scope.search,
+						'label');
+					if (result.length > 0) {
+						document.location.href = result[0].href;
+					}
+				}
+			}
+		};
 
-    _update_time();
-    setInterval(_update_time, 1000);
+		var _update_time = function() {
+			var today = new Date();
+			var h = today.getHours();
+			var m = today.getMinutes();
+			m = m < 10 ? '0' + m : m;
+			$scope.time = h + ":" + m;
+		};
 
-  }]);
+		_update_time();
+		setInterval(_update_time, 1000);
+
+	}]);
