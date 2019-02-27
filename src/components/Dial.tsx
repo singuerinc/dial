37479e8f17319dae200456e090a7813ed6f28648
singuerinc <nahuel.scotti@gmail.com>
2019-02-28 00:01:54 +0100

    feat: sort alpha

--- src/components/Dial.tsx
+++ src/components/Dial.tsx
@@ -6,6 +6,17 @@ import { Search } from "./Search";
 import { Category } from "./Category";
 import { ICategory } from "../ICategory";
 import styled from "styled-components";
+import { ILink } from "../ILink";
+
+// @ts-ignore
+const sorted: ICategory[] = data.default
+  .sort((x: ICategory, y: ICategory) => (x.title > y.title ? 1 : -1))
+  .map((cat: ICategory) => {
+    const links = cat.links.sort((x: ILink, y: ILink) =>
+      x.label > y.label ? 1 : -1
+    );
+    return { ...cat, links };
+  });
 
 const contains = (value: string, label: string) => {
   const labelLower = label.toLocaleLowerCase();
@@ -16,13 +27,11 @@ const contains = (value: string, label: string) => {
 
 export function Dial() {
   const theme = useContext(defaultTheme);
-  // @ts-ignore
-  const initData: ICategory[] = data.default;
-  const [categories, setCategories] = useState<ICategory[]>(initData);
+  const [categories, setCategories] = useState(sorted);
 
   function handleSearchChange(value: string) {
     // select those categories that contains results
-    const filteredCats = initData.filter(cat => {
+    const filteredCats = sorted.filter(cat => {
       return cat.links.some(
         link => contains(value, link.label) || contains(value, link.href)
       );
