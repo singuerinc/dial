efc794903c366776712269175ae621f80b8623fa
singuerinc <nahuel.scotti@gmail.com>
2019-03-06 12:49:56 +0100

    feat: user profile layout

--- src/components/userProfile/UserProfile.tsx
+++ src/components/userProfile/UserProfile.tsx
@@ -8,8 +8,17 @@ interface IProps {
 export function UserProfile({ name, picture }: IProps) {
   return (
     <div className="flex w-33 items-center">
-      <img className="br-100 pa1 ba b--white-10 h3 w3" src={picture} />
-      <h1 className="fw4 ml3">{name}</h1>
+      <article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+        <div className="tc">
+          <img
+            src={picture}
+            className="br-100 h4 w4 dib ba b--black-05 pa2"
+            title={name}
+          />
+          <h1 className="f3 mb2">{name}</h1>
+          <h2 className="f5 fw4 gray mt0">ABC (A B C)</h2>
+        </div>
+      </article>
     </div>
   );
 }
