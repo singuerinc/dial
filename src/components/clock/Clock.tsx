63685ce1a928a193fbc3b921000d3ca6832bdedc
singuerinc <nahuel.scotti@gmail.com>
2019-03-06 21:17:51 +0100

    feat(clock): use rxjs streams

--- src/components/clock/Clock.tsx
+++ src/components/clock/Clock.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
+import { interval } from "rxjs";
 
 const to2 = (x: number) => String(x).padStart(2, "0");
 
@@ -7,9 +8,10 @@ export function Clock() {
   const [date, setDate] = useState(new Date());
 
   useEffect(() => {
-    const update = () => setDate(new Date());
-    const i = setInterval(update, 1000);
-    return () => clearInterval(i);
+    const tick = () => setDate(new Date());
+    const clock$ = interval(1000).subscribe(tick);
+
+    return () => clock$.unsubscribe();
   }, []);
 
   const HH = to2(date.getHours());
