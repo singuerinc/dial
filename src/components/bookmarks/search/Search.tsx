fd7a25f2e6da615dd6487e23c2a3c78d5b37a5a9
Nahuel Scotti <nahuel.scotti@gmail.com>
2020-09-24 16:50:28 +0200

    feat(bookmarks): styles

--- src/components/bookmarks/search/Search.tsx
+++ src/components/bookmarks/search/Search.tsx
@@ -9,39 +9,39 @@ export function Search({ onChange }: IProps) {
   const ref = useRef<HTMLInputElement>(null);
 
   const [value, setValue] = useState<string>("");
-  const [hasFocus, setFocus] = useState<boolean>(false);
+  // const [hasFocus, setFocus] = useState<boolean>(false);
 
-  const handleBlur = () => setFocus(false);
+  // const handleBlur = () => setFocus(false);
 
-  const handleKeyDown = (event: KeyboardEvent) => {
-    if (event.key === "Escape" || (event.key === "/" && !hasFocus)) {
-      event.preventDefault();
-      setValue("");
-      setFocus(true);
-    }
-  };
+  // const handleKeyDown = (event: KeyboardEvent) => {
+  //   if (event.key === "Escape" || (event.key === "/" && !hasFocus)) {
+  //     event.preventDefault();
+  //     setValue("");
+  //     setFocus(true);
+  //   }
+  // };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
-  useEffect(() => {
-    ref.current!.focus();
-  }, [hasFocus]);
+  // useEffect(() => {
+  //   ref.current!.focus();
+  // }, [hasFocus]);
 
   useEffect(() => {
     onChange(value);
   }, [value]);
 
-  useEffect(() => {
-    ref.current!.addEventListener("blur", handleBlur);
-    document.addEventListener("keydown", handleKeyDown);
+  // useEffect(() => {
+  //   ref.current!.addEventListener("blur", handleBlur);
+  //   document.addEventListener("keydown", handleKeyDown);
 
-    return () => {
-      ref.current!.removeEventListener("blur", handleBlur);
-      document.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
+  //   return () => {
+  //     ref.current!.removeEventListener("blur", handleBlur);
+  //     document.removeEventListener("keydown", handleKeyDown);
+  //   };
+  // }, []);
 
   return (
     <input
@@ -49,7 +49,6 @@ export function Search({ onChange }: IProps) {
       className="w-full bg-transparent border-4 focus:border-gray-100 focus:outline-none border-gray-500 px-2 text-4xl -mx-2 my-2 font-light"
       ref={ref}
       value={value}
-      autoFocus
       onChange={handleChange}
     />
   );
