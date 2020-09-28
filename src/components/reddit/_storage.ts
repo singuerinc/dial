9c3b07fe22e911abbaa7df0695ac7be97dfb74fc
Nahuel Scotti <nahuel.scotti@gmail.com>
2020-09-28 16:24:15 +0200

    feat(reddit): feed

---
+++ src/components/reddit/_storage.ts
@@ -0,0 +1,20 @@
+import uniq from "ramda/es/uniq";
+import { IFeedItem } from "./IFeedItem";
+
+export const parse = (s: string) => JSON.parse(s);
+
+export const setViewedInLocalStorage = (stored: number[]) => {
+  const viewed = uniq(stored);
+  localStorage.setItem("reddit/viewed", JSON.stringify(viewed));
+  return viewed;
+};
+
+export const getItemFromLocalStorage = (key: string) => {
+  const value = localStorage.getItem(key) ?? "[]";
+  return value === "[]" ? null : parse(value);
+};
+
+export const saveInLocalStorage = (x: IFeedItem) => {
+  localStorage.setItem(`reddit/item-${x.id}`, JSON.stringify(x));
+  return x;
+};
