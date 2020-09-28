ca7b6974cf50ebd936d53e74ba3afd644053ab3a
Nahuel Scotti <nahuel.scotti@gmail.com>
2020-09-28 10:50:41 +0200

    feat(weather): settings window

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { timer } from "rxjs";
 import { mergeMap } from "rxjs/operators";
 import { IWeather } from "./IWeather";
+import { WeatherSettings, IWeatherInfo } from "./WeatherSettings";
 
 const INTERVAL_UPDATE = 5 * 60000;
 const KEY = localStorage.getItem("weather");
@@ -13,26 +14,36 @@ const pathToDescription = path<string>(["weather", 0, "description"]);
 const pathToIcon = path<number>(["weather", 0, "icon"]);
 const pathToTemp = path<number>(["main", "temp"]);
 
-const loadWeather = async (city: string): Promise<IWeather | null> => {
+const loadWeather = async ({ city, country, unit }: IWeatherInfo): Promise<IWeather | null> => {
   return axios
-    .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}&units=metric`)
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${KEY}&units=${unit}`
+    )
     .then(({ data }) => data)
     .catch(() => null);
 };
 
 export function Weather() {
   const [weather, setWeather] = useState<IWeather>();
-  const city = "Hammarbyhöjden";
+  const [info, setInfo] = useState<IWeatherInfo>({
+    city: "Hammarbyhöjden",
+    country: "Sweden",
+    unit: "metric"
+  });
 
   useEffect(() => {
     const poll$ = timer(0, INTERVAL_UPDATE)
-      .pipe(mergeMap(() => loadWeather(city)))
+      .pipe(mergeMap(() => loadWeather(info)))
       .subscribe((weather: IWeather) => {
         setWeather(weather);
       });
 
     return () => poll$.unsubscribe();
-  }, [city]);
+  }, [info]);
+
+  function onCloseSettings(info: IWeatherInfo) {
+    setInfo(info);
+  }
 
   if (!weather) {
     return null;
@@ -40,10 +51,16 @@ export function Weather() {
 
   return (
     <div className="flex items-center">
+      <WeatherSettings
+        city={info.city}
+        country={info.country}
+        unit={info.unit}
+        onClose={onCloseSettings}
+      />
       <div>
         <h1 className="text-3xl leading-none">{pathToTemp(weather)} °C</h1>
         <h2 className="capitalize">{pathToDescription(weather)}</h2>
-        <h3 className="text-gray-500">{city}</h3>
+        <h3 className="text-gray-500">{info.city}</h3>
       </div>
       <img
         src={`https://openweathermap.org/img/wn/${pathToIcon(weather)}@2x.png`}
---
+++ src/components/weather/WeatherSettings.tsx
@@ -0,0 +1,85 @@
+import { useMachine } from "@xstate/react";
+import * as React from "react";
+import { assign, Machine } from "xstate";
+
+export type IWeatherInfo = {
+  city: string;
+  country: string;
+  unit: "standard" | "metric" | "imperial";
+};
+
+const machine = Machine<IWeatherInfo>(
+  {
+    initial: "idle",
+    context: {
+      city: "New York",
+      country: "useMachine",
+      unit: "standard"
+    },
+    states: {
+      idle: {
+        on: {
+          CITY_UPDATE: { actions: ["cityUpdate"] },
+          UNIT_UPDATE: { actions: ["unitUpdate"] },
+          COUNTRY_UPDATE: { actions: ["countryUpdate"] }
+        }
+      }
+    }
+  },
+  {
+    actions: {
+      cityUpdate: assign({ city: (_, event) => event.value }),
+      unitUpdate: assign({ unit: (_, event) => event.value }),
+      countryUpdate: assign({ country: (_, event) => event.value })
+    }
+  }
+);
+
+export type IProps = IWeatherInfo & {
+  onClose: (info: IWeatherInfo) => void;
+};
+
+export function WeatherSettings({ onClose, city, country, unit }: IProps) {
+  const [state, send] = useMachine(machine, {
+    context: {
+      city,
+      country,
+      unit
+    }
+  });
+
+  console.log(state.context);
+
+  function handleOnClose() {
+    onClose(({ city, country, unit } = state.context));
+  }
+
+  const handleCityChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    send({ type: "CITY_UPDATE", value: event.target.value });
+
+  const handleCountryChange = (event: React.ChangeEvent<HTMLInputElement>) =>
+    send({ type: "COUNTRY_UPDATE", value: event.target.value });
+
+  const handleUnitChange = (event: React.ChangeEvent<HTMLSelectElement>) =>
+    send({ type: "UNIT_UPDATE", value: event.target.value });
+
+  return (
+    <div className="bg-white text-gray-800 p-6 flex flex-wrap flex-col">
+      <i onClick={handleOnClose}>Close</i>
+      <h1 className="text-2xl font-bold">Weather</h1>
+      <form className="w-full">
+        <h2 className="text-lg font-bold">City</h2>
+        <input value={state.context.city} onChange={handleCityChange} />
+
+        <h2 className="text-lg font-bold">Country</h2>
+        <input value={state.context.country} onChange={handleCountryChange} />
+        <h2 className="text-lg font-bold">Units</h2>
+        <select value={state.context.unit} onChange={handleUnitChange}>
+          <option value="standard">Standard</option>
+          <option value="metric">Metric</option>
+          <option value="imperial">Imperial</option>
+        </select>
+      </form>
+    </div>
+  );
+}
