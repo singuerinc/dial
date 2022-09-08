8f4799af252440c1ce0a017cca672698c1125ca5
Nahuel Scotti <nahuel.scotti@gmail.com>
2022-09-08 12:44:14 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { bookmarks } from "./bookmarks/data";
 function App() {
   return (
     <div className="mx-6 md:mx-32 md:my-16">
-      <div className="my-8 flex md:my-16">
+      <!--div className="my-8 flex md:my-16">
         <h1 className="text-3xl font-light">dial</h1>
-      </div>
+      </div-->
       <Bookmarks bookmarks={bookmarks} />
     </div>
   );
